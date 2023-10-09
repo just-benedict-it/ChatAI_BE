16b7eb753126d23ed0b27b949b499ff65c377d74
just-benedict-it <shsms9223@gmail.com>
2023-10-09 22:56:14 +0900

    secrets.json to .env

--- database.py
+++ database.py
@@ -3,6 +3,16 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 import json
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+user_name = os.getenv("USER_NAME")
+password = os.getenv("PASSWORD")
+host = os.getenv("HOST")
+port = os.getenv("PORT")
+database = os.getenv("DATABASE")
+
+print(user_name, password, host, port, database)
 
 #MySQL 연결할 때 쓰는 코드
 ## print(__file__) #이 파일의 상대경로
@@ -13,14 +23,9 @@ import os
 
 # DB_URL = f"mysql+pymysql://{DB['user']}:{DB['password']}@{DB['host']}:{DB['port']}/{DB['database']}?charset=utf8"
 
-#MySQL 연결할 때 쓰는 코드
-## print(__file__) #이 파일의 상대경로
-BASE_DIR = os.path.dirname(os.path.abspath(__file__)) #이 파일의 절대경로
-SECRET_FILE = os.path.join(BASE_DIR, 'secrets.json')
-secrets = json.loads(open(SECRET_FILE).read())
-DB = secrets["DB"]
-
-DB_URL = f"mysql+pymysql://{DB['user']}:{DB['password']}@{DB['host']}:{DB['port']}/{DB['database']}?charset=utf8"
+#MariaDB 연결할 때 쓰는 코드
+DB_URL = f"mysql+pymysql://{user_name}:{password}@{host}:{port}/{database}?charset=utf8"
+# DB_URL = f"mysql+pymysql://{DB['user']}:{DB['password']}@{DB['host']}:{DB['port']}/{DB['database']}?charset=utf8"
 
 # 데이터베이스 설정
 # DB_URL = "sqlite:///./google_chatgpt.db?check_same_thread=False"
